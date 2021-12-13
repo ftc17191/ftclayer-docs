8920058181b1c00011097cecba67c8f70ddeda2d
jackcrane <jack-mac@users.noreply.github.com>
2021-12-13 18:10:15 -0500

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,98 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'FTCLayer documentation',
+  tagline: 'Make FTC programming easy',
+  url: 'https://ftclayer-docs.pages.dev',
+  baseUrl: 'FTCLayer-website/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ftc17191', // Usually your GitHub org/user name.
+  projectName: 'FTCLayer-website', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/ftc17191/FTCLayer-website/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'FTCLayer',
+        logo: {
+          alt: 'FIRST Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Getting Started',
+          },
+          {
+            href: 'https://github.com/ftc17191/FTCLayer-website',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ftc17191/FTCLayer-website',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} FTCLayer. Built with Docusaurus. Hosted on Cloudflare Pages`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
